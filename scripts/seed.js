cd24e3efd37ea6172b7b42aab08c2f84104a547f
Chris Cooper <cjcooper252@gmail.com>
2024-01-12 18:02:25 -0800

    Add seed data

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,5 +1,9 @@
 const { db } = require("@vercel/postgres");
-const { programs } = require("../src/app/lib/placeholder-data.js");
+const {
+  programs,
+  days,
+  exercises,
+} = require("../src/app/lib/placeholder-data.js");
 
 async function seedPrograms(client) {
   try {
@@ -37,10 +41,83 @@ async function seedPrograms(client) {
   }
 }
 
+async function seedDays(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS days (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        program_id UUID NOT NULL,
+        name VARCHAR(50) NOT NULL
+      );
+    `;
+
+    console.log(`Created "days" table`);
+
+    const insertedDays = await Promise.all(
+      days.map(async (day) => {
+        return client.sql`
+        INSERT INTO days (id, program_id, name)
+        VALUES (${day.id}, ${day.progam_id} ,${day.name})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+      }),
+    );
+
+    console.log(`Seeded ${insertedDays.length} days`);
+
+    return {
+      createTable,
+      programs: insertedDays,
+    };
+  } catch (error) {
+    console.error("Error seeding days:", error);
+    throw error;
+  }
+}
+
+async function seedExercises(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS exercises (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        day_id UUID NOT NULL,
+        name VARCHAR(50) NOT NULL,
+        description TEXT NOT NULL
+      );
+    `;
+
+    console.log(`Created "exercises" table`);
+
+    const insertedExercises = await Promise.all(
+      exercises.map(async (exercise) => {
+        return client.sql`
+        INSERT INTO exercises (id, day_id, name, description)
+        VALUES (${exercise.id}, ${exercise.day_id} ,${exercise.name}, ${exercise.description})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+      }),
+    );
+
+    console.log(`Seeded ${insertedExercises.length} exercises`);
+
+    return {
+      createTable,
+      programs: insertedExercises,
+    };
+  } catch (error) {
+    console.error("Error seeding exercises:", error);
+    throw error;
+  }
+}
+
 async function main() {
   const client = await db.connect();
 
   await seedPrograms(client);
+  await seedDays(client);
+  await seedExercises(client);
 
   await client.end();
 }
--- src/app/lib/placeholder-data.js
+++ src/app/lib/placeholder-data.js
@@ -5,6 +5,52 @@ const programs = [
   },
 ];
 
+const days = [
+  {
+    id: "5dd9cd5f-3331-4823-958d-41bf97b5733b",
+    progam_id: programs[0].id,
+    name: "Day 1",
+  },
+  {
+    id: "522f479c-2922-4120-8f0b-838f929ebb48",
+    progam_id: programs[0].id,
+    name: "Day 2",
+  },
+  {
+    id: "97c30dac-a141-4b06-b84f-cfae3c90613b",
+    progam_id: programs[0].id,
+    name: "Day 3",
+  },
+  {
+    id: "198af334-8913-4b92-915f-a4e49e2bd9a9",
+    progam_id: programs[0].id,
+    name: "Day 4",
+  },
+];
+
+const exercises = [
+  {
+    id: "f62f295e-99ca-459f-ac50-5e7c46ad29ba",
+    day_id: days[0].id,
+    name: "A. Squat Jump",
+    description: "3 set of 5 reps, 30 seconds between sets",
+  },
+  {
+    id: "d11b3927-7127-461a-8480-224c20bd799b",
+    day_id: days[0].id,
+    name: "B1. Goblet Squats",
+    description: "4 sets of 6 at a 7 RPE. Superset with B2",
+  },
+  {
+    id: "8ba163e3-9cb4-4fe0-8a9e-3212c8f1b3e3",
+    day_id: days[0].id,
+    name: "B2. 1DB Seated External Rotation",
+    description: "3 sets of 10 each side at a 6-7 RPE",
+  },
+];
+
 module.exports = {
   programs,
+  days,
+  exercises,
 };
