8cb447d7f13126196cca8396e716b9f0c048caef
Chris Cooper <cjcooper252@gmail.com>
2024-01-25 16:28:59 -0800

    use shadcn card instead of custom

--- app/workouts/page.tsx
+++ app/workouts/page.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
 import { fetchIncompleteSessions, fetchWorkouts } from "@/lib/data";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default async function Page() {
   const workouts = await fetchWorkouts();
@@ -13,21 +19,21 @@ export default async function Page() {
           Continue an active session
         </p>
       </div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+
+      <div className="flex flex-col gap-4">
         {incompleteSessions.map((session) => (
-          <div
-            key={session.id}
-            className="group relative rounded-lg border border-gray-200 bg-white p-4 shadow-sm transition-colors hover:bg-gray-100  dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800"
-          >
-            <Link
-              className="absolute inset-0"
-              href={`sessions/${session.id}`}
-            />
-            <h3 className="text-lg font-semibold">{session.workout.name}</h3>
-            <p className="text-gray-500 dark:text-gray-400">
-              {`Started on ${session.workout.createdAt?.toDateString()}`}
-            </p>
-          </div>
+          <Link key={session.id} href={`/sessions/${session.id}`}>
+            <Card className="transition-colors hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800">
+              <CardHeader className="p-4">
+                <CardTitle className="text-lg">
+                  {session.workout.name}
+                </CardTitle>
+                <CardDescription>
+                  {`Started on ${session.workout.createdAt?.toDateString()}`}
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </Link>
         ))}
       </div>
       <div className="flex flex-col gap-2">
@@ -36,23 +42,20 @@ export default async function Page() {
           Choose a workout to start your session
         </p>
       </div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="flex flex-col gap-4">
         {workouts.map((workout) => (
-          <div
-            key={workout.id}
-            className="group relative rounded-lg border border-gray-200 bg-white p-4 shadow-sm transition-colors hover:bg-gray-100 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800"
-          >
-            <Link
-              className="absolute inset-0"
-              href={`workouts/${workout.id}`}
-            />
-            <h3 className="text-lg font-semibold">{workout.name}</h3>
-            <p className="text-gray-500 dark:text-gray-400">
-              {workout.lastCompleted
-                ? `Last completed on ${workout.lastCompleted.toDateString()}`
-                : null}
-            </p>
-          </div>
+          <Link key={workout.id} href={`/workouts/${workout.id}`}>
+            <Card className="transition-colors hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800">
+              <CardHeader className="p-4">
+                <CardTitle className="text-lg">{workout.name}</CardTitle>
+                {workout.lastCompleted ? (
+                  <CardDescription>
+                    {`Last completed on ${workout.lastCompleted.toDateString()}`}
+                  </CardDescription>
+                ) : null}
+              </CardHeader>
+            </Card>
+          </Link>
         ))}
       </div>
       <div className="flex justify-end">
