ce60282e93e6310e6fd23ba8b439fb3122b9fcb5
Chris Cooper <cjcooper252@gmail.com>
2024-02-09 12:13:04 -0800

    full page background

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,14 +18,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-col">
+        <div className="flex h-screen flex-col">
           <header className="flex h-16 items-center border-b px-4 md:px-6">
             <Link className="flex items-center gap-2" href="/">
               <MountainIcon className="h-6 w-6" />
               <span className="text-lg font-semibold">Workout Tracker</span>
             </Link>
           </header>
-          <main className="bg-gray-100 p-4 dark:bg-gray-900 md:p-6">
+          <main className="flex-1 bg-gray-100 p-4 dark:bg-gray-900 md:p-6">
             {children}
           </main>
         </div>
